6f730ccd38e3dde549802e6ed17bfb00c94a323d
Joseph <jxlove2020@gmail.com>
2022-07-12 21:48:49 +0900

    unsubscribe() 구독 취소

--- src/index.js
+++ src/index.js
@@ -6,14 +6,20 @@ import reportWebVitals from './reportWebVitals';
 import store from './redux/store';
 import { addTodo } from './redux/actions';
 
-// 구독하게 되면 state값이 변경 될 때 마다 호출
-store.subscribe(() => {
+// 구독하게 되면 state값이 변경 될 때(dispatch 될 때) 마다 호출 , return 값이 unsubscribe
+// 구독 취소는 unsubscribe()
+const unsubscribe = store.subscribe(() => {
   console.log(store.getState())
 })
 
 store.dispatch(addTodo('책 읽기'))
 store.dispatch(addTodo('캐치볼 하기'))
 store.dispatch(addTodo('줄넘기 하기'))
+unsubscribe() // 구독 취소는 했지만 State 값은 변경되어 저장 된다. subscribe 함수 내의 동작만 하지 않는 것
+store.dispatch(addTodo('피아노 연주'))
+store.dispatch(addTodo('기타 연주'))
+store.dispatch(addTodo('그림 그리기'))
+console.log(store.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
