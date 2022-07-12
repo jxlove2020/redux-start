6d33b6733b7965652294c16492a277bdf13c7d76
Joseph <jxlove2020@gmail.com>
2022-07-12 21:30:08 +0900

    스토어 생성

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+
+console.log(store)
+console.log(store.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,7 +3,7 @@ import { ADD_TODO } from './actions'
 // 초기값 설정 다른 방법
 const initialState = [];
 
-function todoApp(previousState = initialState, action) {
+export function todoApp(previousState = initialState, action) {
   // // 초기값 설정해주는 부분
   // if (previousState === undefined) {
   //   return []
