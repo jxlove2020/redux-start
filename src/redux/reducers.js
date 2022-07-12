451a3f950ea7885e0521504237f2bc2719f09c00
Joseph <jxlove2020@gmail.com>
2022-07-12 21:05:10 +0900

    초기값 설정 다른 방법

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,9 +1,12 @@
 import { ADD_TODO } from './actions'
-function todoApp(previousState, action) {
-  // 초기값 설정해주는 부분
-  if (previousState === undefined) {
-    return []
-  }
+
+const initialState = [];
+
+function todoApp(previousState = initialState, action) {
+  // // 초기값 설정해주는 부분
+  // if (previousState === undefined) {
+  //   return []
+  // }
 
   if (action.type === ADD_TODO) {
     return [...previousState, action.todo]
