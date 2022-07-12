c24b8ba0632fec330a39a609600a6cf09d9cb9d0
Joseph <jxlove2020@gmail.com>
2022-07-12 22:11:26 +0900

    complete 액션 추가

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,9 +1,17 @@
 export const ADD_TODO = 'ADD_TODO';
+export const COMPLETE_TODO = 'COMPLETE_TODO'
 
 // {type: ADD_TODO, text: '할일'}
 export function addTodo(text) {
   return {
     type: ADD_TODO,
     text,
+  }
+}
+
+export function completeTodo(index) {
+  return {
+    type: COMPLETE_TODO,
+    index,
   }
 }
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,4 @@
-import { ADD_TODO } from './actions'
+import { ADD_TODO, COMPLETE_TODO } from './actions'
 
 // 초기값 설정 다른 방법
 const initialState = [];
@@ -11,6 +11,15 @@ export function todoApp(previousState = initialState, action) {
 
   if (action.type === ADD_TODO) {
     return [...previousState, { text: action.text, done: false }]
+  }
+
+  if (action.type === COMPLETE_TODO) {
+    return previousState.map((todo, index) => {
+      if (index === action.index) {
+        return { ...todo, done: true }
+      }
+      return todo
+    })
   }
 
   return previousState
