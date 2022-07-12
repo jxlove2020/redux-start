50b7878e5e38d4704efe6d59c069287bd453f35a
Joseph <jxlove2020@gmail.com>
2022-07-12 22:21:24 +0900

    state형태 배열에서 객체 형태로 변경

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,25 +1,27 @@
 import { ADD_TODO, COMPLETE_TODO } from './actions'
 
 // 초기값 설정 다른 방법
-const initialState = [];
+const initialState = { todos: [], filter: 'ALL' };
 
 export function todoApp(previousState = initialState, action) {
-  // // 초기값 설정해주는 부분
-  // if (previousState === undefined) {
-  //   return []
-  // }
 
   if (action.type === ADD_TODO) {
-    return [...previousState, { text: action.text, done: false }]
+    return {
+      ...previousState,
+      todos: [...previousState.todos, { text: action.text, done: false }]
+    }
   }
 
   if (action.type === COMPLETE_TODO) {
-    return previousState.map((todo, index) => {
-      if (index === action.index) {
-        return { ...todo, done: true }
-      }
-      return todo
-    })
+    return {
+      ...previousState,
+      todos: previousState.todos.map((todo, index) => {
+        if (index === action.index) {
+          return { ...todo, done: true }
+        }
+        return todo
+      })
+    }
   }
 
   return previousState
