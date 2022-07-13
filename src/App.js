7035e21eff427824c4a62457b1b8dd9fa2527909
Joseph <jxlove2020@gmail.com>
2022-07-14 06:07:23 +0900

    커스텀 훅 정리

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
-import { useContext, useEffect, useState } from 'react';
 import './App.css';
-import ReduxContext from './contexts/ReduxContext';
+import useReduxState from './hooks/useReduxState';
+import useReduxDispatch from './hooks/useReduxDispatch';
 import { addTodo } from './redux/actions';
-
-function useReduxState() {
-  const store = useContext(ReduxContext)
-  const [state, setState] = useState(store.getState())
-  useEffect(() => {
-    const unsubscribe = store.subscribe(() => {
-      setState(store.getState())
-    })
-    return () => { unsubscribe() }
-  }, [store])
-
-  return state
-}
-
-function useReduxDispatch() {
-  const store = useContext(ReduxContext)
-  return store.dispatch
-}
 
 function App() {
   const state = useReduxState()
